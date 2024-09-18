5dd75dbd76599c6a3561febfc29affe3b37c7f3f
mdrifat59 <mdrifatulislam59@gmail.com>
2024-09-18 10:18:51 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1 className='text-red-500 text-center'>this ss me</h1>
+    </>
+  )
+}
+
+export default App
