6f2136b3a53e1f8b735a55e73d59b0d2fffbb816
mdrifat59 <mdrifatulislam59@gmail.com>
2024-10-03 12:27:17 +0600

    Message box design is done

--- src/components/chatbox/Chattingbox.jsx
+++ src/components/chatbox/Chattingbox.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Audioicon } from '../../svg/Audio'
 import { EmojiIcon } from '../../svg/Emoji'
 import { PictuerIcon } from '../../svg/Pictuer'
+import checkpoto from '../../../public/check.jpg'
+import checkpoto2 from '../../../public/check2.jpg'
 
 const Chattingbox = () => {
   return (
@@ -10,6 +12,46 @@ const Chattingbox = () => {
         <div className='flex items-center p-5 gap-5 bg-[#F9F9F9]'>
           <div className='w-[81px] h-[81px] bg-gray-500 rounded-full' ></div>
           <h3 className='font-inter_medium text-xl text-[#000000]'>MD Rifatul Islam</h3>
+        </div>
+        <div className="h-[500px] px-5 overflow-y-auto">
+          {/* sender msg */}
+          <div className="w-[65%] ml-auto flex flex-col items-end">
+            <p className="bg-sky-400 py-3 px-3 rounded-lg inline-block mt-5 text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium quod debitis ex dolore sed expedita natus architecto accusantium autem voluptate totam repellendus, quasi maiores aliquid rem quo deleniti soluta aut? </p>
+            <span  >date</span>
+          </div>
+          {/* sender img */}
+          <div className="w-[65%] ml-auto flex flex-col items-end">
+            <div className=" py-3 px-3  inline-block mt-5">
+              <img src={checkpoto} className='border border-sky-400 rounded-lg object-cover w-full h-full' alt="" />
+            </div>
+            <span >date</span>
+          </div>
+          {/* sender audio */}
+          <div className="w-[65%] ml-auto flex flex-col items-end">
+            <div className=" py-3 px-3  inline-block mt-5">
+              <audio src="" controls></audio>
+            </div>
+            <span  >date</span>
+          </div>
+          {/* receiver msg */}
+          <div className="w-[65%] ">
+            <p className="  bg-slate-400 py-3 px-3 rounded-lg inline-block mt-5 text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium quod debitis ex dolore sed expedita natus architecto accusantium autem voluptate totam repellendus, quasi maiores aliquid rem quo deleniti soluta aut? </p>
+            <span  >date</span>
+          </div>
+          {/* receiver img */}
+          <div className="w-[65%] ">
+            <div className=" py-3 px-3  inline-block mt-5">
+              <img src={checkpoto2} className='border border-slate-400 rounded-lg object-cover w-full h-full' alt="" />
+            </div>
+            <span >date</span>
+          </div>
+          {/* receiver audio */}
+          <div className="w-[65%] ">
+            <div className=" py-3 px-3  inline-block mt-5">
+              <audio src="" controls></audio>
+              <span >date</span>
+            </div>
+          </div>
         </div>
         <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-[85%] py-4 px-2  bg-[#F5F5F5] rounded-lg grid grid-cols-[2fr,6fr,1fr]'>
           <div className='w-full h-full flex justify-center items-center gap-4'>
