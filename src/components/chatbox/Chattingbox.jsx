1c295efb2c70e3ad494658f799eee63ca20f3ddb
mdrifat59 <mdrifatulislam59@gmail.com>
2024-10-21 01:48:01 +0600

    blocked problem not solve

--- src/components/chatbox/Chattingbox.jsx
+++ src/components/chatbox/Chattingbox.jsx
@@ -18,11 +18,13 @@ const Chattingbox = () => {
   let user = useSelector((state) => state.login.loggedIn)
   let [message, setMessage] = useState("")
   let [allmessage, setAllmessagea] = useState([])
-  // let [block, setBlock] = useState([])
   let [emojishow, setEmojishow] = useState(false)
   let [selectedimg, setSelectedimg] = useState(null)
   let [imagefile, setImagefile] = useState(null)
   let [audiourl, setAudiourl] = useState("")
+  let [friends, setfriends] = useState([])
+  let [block, setBlock] = useState([])
+  let [lala, setLala] = useState([])
   let [blobs, setBlobs] = useState("")
   let choosefile = useRef(null)
   let scrollRef = useRef()
@@ -148,17 +150,34 @@ const Chattingbox = () => {
       });
     });
   }
-  // blokes
-  // useEffect(()=>{
-  //   const blockdRef = ref(db, 'blocks/');
-  //     onValue(blockdRef, (snapshot) => {
-  //        let blockarr = []
-  //        snapshot.forEach((item) => {
-  //           blockarr.push({ ...item.val(), id: item.key })
-  //        })
-  //        setBlock(blockarr)
-  //     });
-  // },[db])
+  // console.log(allmessage.some((e)=>e.whoreceiveid));
+  useEffect(() => {
+    const blockdRef = ref(db, 'blocks/');
+    onValue(blockdRef, (snapshot) => {
+      let blockarr = []
+      // let incluarr = []
+      snapshot.forEach((item) => {  
+          blockarr.push({ ...item.val(), id: item.key }) 
+      })
+      setBlock(blockarr)
+      // setLala(incluarr)
+    });
+
+  }, [singlefriend.id])
+  console.log(block);
+  
+  useEffect(() => {
+    const blockdRef = ref(db, 'friends/');
+    onValue(blockdRef, (snapshot) => {
+      let friendarr = []
+      snapshot.forEach((item) => {
+        friendarr.push({ ...item.val(), id: item.key })
+      })
+      setfriends(friendarr)
+    });
+
+  }, [db])
+
   return (
     <>
       <div className='w-full h-full relative'>
@@ -167,6 +186,7 @@ const Chattingbox = () => {
             <img src={singlefriend?.profile || avaterimg} className='w-full h-full rounded-full object-cover overflow-hidden' alt="" />
           </div>
           <h3 className='font-inter_medium text-xl text-[#000000]'>{singlefriend?.name || "Choouse your friend"}</h3>
+          <p>{singlefriend.id}</p>
         </div>
         <div className="h-[500px] px-5 overflow-y-auto">
           {
@@ -174,6 +194,7 @@ const Chattingbox = () => {
               allmessage.map((item, index) => (
                 <div key={index} ref={scrollRef}>
                   {
+
                     item.whosendid == user.uid ? (
                       item.audio ?
                         <div className="w-[65%] ml-auto flex flex-col items-end">
@@ -223,73 +244,83 @@ const Chattingbox = () => {
               ))
               : ""
           }
+          {/* second breakets */}
 
-        </div>
-        
-        <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-[85%] py-4 px-2  bg-[#F5F5F5] rounded-lg grid grid-cols-[2fr,6fr,1fr]'>
-          <div className='w-full h-full flex justify-center items-center gap-4'>
-            <div className='cursor-pointer ' onClick={() => recorderControls.startRecording()}>
-              <Audioicon />
-            </div>
-            <div className=' flex justify-center items-center gap-2'>
-              <div className='relative'>
-                <div className='cursor-pointer' onClick={() => setEmojishow((prev) => !prev)}>
-                  <EmojiIcon />
-                </div>
-                {
-                  emojishow &&
-                  <div className='absolute bottom-9 -left-24'>
-                    <EmojiPicker onEmojiClick={handleEmoji} />
-                  </div>
-                }
-              </div>
-            </div>
-            <div onClick={() => choosefile.current.click()} className='cursor-pointer'>
-              <PictuerIcon />
-              <input type="file" ref={choosefile} hidden onChange={handleImage} />
-            </div>
-          </div>
-          <div className='w-full h-full flex items-center '>
-            {
-              !recorderControls.isRecording && !audiourl &&
-              <input type="text" className='w-full py-3 px-2 outline-none rounded-lg font-semibold' onKeyUp={handleSendKey} value={message} onChange={(e) => setMessage(e.target.value)} placeholder='type here...' />
-            }
-            <div className={`mx-auto ${!recorderControls.isRecording ? "hidden" : ""}`}>
-              <AudioRecorder
-                onRecordingComplete={(blob) => addAudioElement(blob)}
-                recorderControls={recorderControls}
-                showVisualizer={true}
-              />
-            </div>
-            {
-              audiourl &&
-              <div className='flex gap-5 mx-auto'>
-                <audio src={audiourl} controls className='border border-black rounded-lg'></audio>
-                <button className='py-2 px-5 bg-red-500 text-white rounded-lg' onClick={() => setAudiourl("")}>Cencel</button>
-              </div>
-            }
-          </div>
-          <div className='absolute bottom-1/2 left-1/4'>
-            {
-              selectedimg &&
-              <>
-                <img src={selectedimg} className='w-[300px] h-[300px] object-cover overflow-hidden relative' alt="" />
-                <div className='absolute top-2 right-2 text-red-500 cursor-pointer' onClick={() => setSelectedimg(null)}>
-                  <Closeicons />
-                </div>
-              </>
-            }
-          </div>
-          <div className='w-full h-full '>
-            {
-              audiourl ?
-                <button className="font-inter_medium  text-xl  bg-[#3E8DEB] text-[#FFFFFF] py-3 px-10 rounded-lg mx-2" onClick={handleSendaudio} >Send</button>
-                :
-                <button className="font-inter_medium  text-xl bg-[#3E8DEB] text-[#FFFFFF]   py-3 px-10 rounded-lg mx-2" onClick={handleSend} >Send</button>
-            }
-
-          </div>
         </div> 
+ 
+         
+         {singlefriend !== null && singlefriend?.isblocked && (
+          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-[85%] py-4 px-2  bg-[#F5F5F5] rounded-lg '>
+            <p className='text-black bg-red-500'>This chat is blocked  </p>
+          </div>
+        )} 
+         {singlefriend !== null && !singlefriend?.isblocked && (
+          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-[85%] py-4 px-2  bg-[#F5F5F5] rounded-lg grid grid-cols-[2fr,6fr,1fr]'>
+            <div className='w-full h-full flex justify-center items-center gap-4'>
+              <div className='cursor-pointer ' onClick={() => recorderControls.startRecording()}>
+                <Audioicon />
+              </div>
+              <div className=' flex justify-center items-center gap-2'>
+                <div className='relative'>
+                  <div className='cursor-pointer' onClick={() => setEmojishow((prev) => !prev)}>
+                    <EmojiIcon />
+                  </div>
+                  {
+                    emojishow &&
+                    <div className='absolute bottom-9 -left-24'>
+                      <EmojiPicker onEmojiClick={handleEmoji} />
+                    </div>
+                  }
+                </div>
+              </div>
+              <div onClick={() => choosefile.current.click()} className='cursor-pointer'>
+                <PictuerIcon />
+                <input type="file" ref={choosefile} hidden onChange={handleImage} />
+              </div>
+            </div>
+            <div className='w-full h-full flex items-center '>
+              {
+                !recorderControls.isRecording && !audiourl &&
+                <input type="text" className='w-full py-3 px-2 outline-none rounded-lg font-semibold' onKeyUp={handleSendKey} value={message} onChange={(e) => setMessage(e.target.value)} placeholder='type here...' />
+              }
+              <div className={`mx-auto ${!recorderControls.isRecording ? "hidden" : ""}`}>
+                <AudioRecorder
+                  onRecordingComplete={(blob) => addAudioElement(blob)}
+                  recorderControls={recorderControls}
+                  showVisualizer={true}
+                />
+              </div>
+              {
+                audiourl &&
+                <div className='flex gap-5 mx-auto'>
+                  <audio src={audiourl} controls className='border border-black rounded-lg'></audio>
+                  <button className='py-2 px-5 bg-red-500 text-white rounded-lg' onClick={() => setAudiourl("")}>Cencel</button>
+                </div>
+              }
+            </div>
+            <div className='absolute bottom-1/2 left-1/4'>
+              {
+                selectedimg &&
+                <>
+                  <img src={selectedimg} className='w-[300px] h-[300px] object-cover overflow-hidden relative' alt="" />
+                  <div className='absolute top-2 right-2 text-red-500 cursor-pointer' onClick={() => setSelectedimg(null)}>
+                    <Closeicons />
+                  </div>
+                </>
+              }
+            </div>
+            <div className='w-full h-full '>
+              {
+                audiourl ?
+                  <button className="font-inter_medium  text-xl  bg-[#3E8DEB] text-[#FFFFFF] py-3 px-10 rounded-lg mx-2" onClick={handleSendaudio} >Send</button>
+                  :
+                  <button className="font-inter_medium  text-xl bg-[#3E8DEB] text-[#FFFFFF]   py-3 px-10 rounded-lg mx-2" onClick={handleSend} >Send</button>
+              }
+
+            </div>
+          </div> 
+        ) 
+      } 
       </div>
     </>
   )
